ddd8ba013a3e10666993ac68301528dfb1da9ea6
“Nicole <itsnicoleduffy@gmail.com>
2023-10-17 16:24:17 +0200

    edited objectid

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -15,7 +15,7 @@ const studentSchema = new Schema({
   program: {type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
   background: {type: String, default: ""},
   image: {type: String, default: "https://i.imgur.com/r8bo8u7.png"},
-  cohort: ObjectId,
+  cohort: {type:ObjectId, ref:"Cohort"},
   projects: Array,
 });
 
